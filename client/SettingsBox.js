7f2dc667de7fe1ce66bfd60b752bf00bd45689c5
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-03-20 22:25:09 -0400

    tweak design

--- client/SettingsBox.js
+++ client/SettingsBox.js
@@ -7,7 +7,7 @@ const SettingsBox = (props) => {
         <div id='settings-box'>
             <h2>Settings</h2>
             <form>
-                <input value={props.year} min='1890' max="2022" id='year' name='year' type='range' onChange={props.handleYearChange}></input>
+                <input value={props.year} min='1890' max="2020" id='year' name='year' type='range' onChange={props.handleYearChange}></input>
                 <span>Show me the top<input value={props.pop} min='1' max='99' type='number' onChange={props.handlePopChange}></input>% of names that were popular in {props.year} for
                 <select name='gender' onChange={props.handleSelect}>
                     <option defaultValue="anyone">anyone</option>
