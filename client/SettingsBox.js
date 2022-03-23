006968ff4f7c48624693dffeb36b6be8b2d35ae1
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-03-22 21:06:47 -0400

    update welcome logo and settings box design

--- client/SettingsBox.js
+++ client/SettingsBox.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const SettingsBox = (props) => {
-    // const slider = document.getElementById('year')
-    // console.log(slider);
-    return (
-        <div id='settings-box'>
-            <h2>Settings</h2>
-            <form>
-                <input value={props.year} min='1890' max="2020" id='year' name='year' type='range' onChange={props.handleYearChange}></input>
-                <span>Show me the top<input value={props.pop} min='1' max='99' type='number' onChange={props.handlePopChange}></input>% of names that were popular in {props.year} for
-                <select name='gender' onChange={props.handleSelect}>
-                    <option defaultValue="anyone">anyone</option>
-                    <option value="girls">girls</option>
-                    <option value="boys">boys</option>
-                </select></span>
-                <input id='submit-settings' type='submit' onClick={props.submitHandler}></input>
-            </form>
-        </div>
-    )
-}
+  // const slider = document.getElementById('year')
+  // console.log(slider);
+  return (
+    <div id="settings-box">
+      <h2>Settings</h2>
+      <form>
+        <input
+          value={props.year}
+          min="1890"
+          max="2020"
+          id="year"
+          name="year"
+          type="range"
+          onChange={props.handleYearChange}
+        ></input>
+        <span>
+          Show me the top
+          <input
+            value={props.pop}
+            min="1"
+            max="99"
+            type="number"
+            onChange={props.handlePopChange}
+          ></input>
+          % of names that were popular in {props.year} for
+          <select name="gender" onChange={props.handleSelect}>
+            <option defaultValue="anyone">anyone</option>
+            <option value="girls">girls</option>
+            <option value="boys">boys</option>
+          </select>
+        </span>
+        <input
+          id="submit-settings"
+          type="submit"
+          onClick={props.submitHandler}
+        ></input>
+      </form>
+    </div>
+  );
+};
 
 export default SettingsBox;
--- client/Welcome.js
+++ client/Welcome.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Welcome = (props) => {
-    return (
-        <div id='welcome-box' onClick={props.clickHandler}>
-            <h1 id='logo-top'>EENIE</h1>
-            <h1 id='logo-bottom'>MEENIE</h1>
-        </div>
-    )
-}
+  return (
+    <div id="welcome-box" onClick={props.clickHandler}>
+      <div id="main-logo">
+        <h1 id="logo-top">EENIE</h1>
+        <h1 id="logo-bottom">MEENIE</h1>
+      </div>
+      <div id="under-logo">
+        <h1>Baby Name Game</h1>
+      </div>
+    </div>
+  );
+};
 
 export default Welcome;
